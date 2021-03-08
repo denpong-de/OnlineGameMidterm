7c5ea42ab6a3ce635d7642d3f77daad6dd4f1505
Denpong_Denjarukul <denpong_de@users.noreply.Bitbucket.com>
2021-03-08 12:35:26 +0700

    Add Input Action and CarController Script
    CarController can get input form input action but is not interactable with car in game yet (Basic Debug.log).

---
+++ MidtermOnlineRacing_Project/Assets/!Scripts/CarController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CarController : MonoBehaviour
+{
+    private Vector2 moveVec;
+    private bool isBreaking;
+
+    [SerializeField] private float motorForce;
+    [SerializeField] private float breakForce;
+
+    [SerializeField] private WheelCollider frontLeftCollider;
+    [SerializeField] private WheelCollider frontRightCollider;
+    [SerializeField] private WheelCollider rearLeftCollider;
+    [SerializeField] private WheelCollider rearRightCollider;
+
+    private void Awake()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        //HandleMotor();
+        //if (Keyboard.current.spaceKey.isPressed)
+        //{
+        //    //ApplyBreaking();
+        //    Debug.Log("isBereaking");
+        //}
+        Debug.Log(isBreaking);
+    }
+
+    private void HandleMotor()
+    {
+        frontLeftCollider.motorTorque = moveVec.y * motorForce;
+        frontRightCollider.motorTorque = moveVec.y * motorForce;
+
+    }
+
+    private void ApplyBreaking()
+    {
+        frontLeftCollider.brakeTorque = breakForce;
+        frontRightCollider.brakeTorque = breakForce;
+        rearLeftCollider.brakeTorque = breakForce;
+        rearRightCollider.brakeTorque = breakForce;
+    }
+
+    public void OnBreak(InputValue value)
+    {
+        float breakValue = value.Get<float>();
+
+        if(breakValue == 1)
+        {
+            isBreaking = true;
+        }
+        else
+        {
+            isBreaking = false;
+        }
+    }
+
+    public void OnMove(InputValue value)
+    {
+        moveVec = value.Get<Vector2>();
+    }
+}
