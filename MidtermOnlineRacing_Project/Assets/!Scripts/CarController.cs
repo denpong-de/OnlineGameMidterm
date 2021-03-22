67171de98f6fcdc4c0d0bc8b831b6e99c1030628
Denpong_Denjarukul <denpong_de@users.noreply.Bitbucket.com>
2021-03-22 12:34:55 +0700

    Finished CarController but need some adjustment and init CamController.
    + Car is now Prefab and increase specular in car materials.

--- MidtermOnlineRacing_Project/Assets/!Scripts/CarController.cs
+++ MidtermOnlineRacing_Project/Assets/!Scripts/CarController.cs
@@ -6,37 +6,38 @@ using UnityEngine.InputSystem;
 public class CarController : MonoBehaviour
 {
     private Vector2 moveVec;
+    private float currentSteerAngle;
     private bool isBreaking;
 
     [SerializeField] private float motorForce;
     [SerializeField] private float breakForce;
+    [SerializeField] private float maxSteerAngle;
 
     [SerializeField] private WheelCollider frontLeftCollider;
     [SerializeField] private WheelCollider frontRightCollider;
     [SerializeField] private WheelCollider rearLeftCollider;
     [SerializeField] private WheelCollider rearRightCollider;
 
-    private void Awake()
-    {
-        
-    }
+    [SerializeField] private Transform frontLeftTransform;
+    [SerializeField] private Transform frontRightTransform;
+    [SerializeField] private Transform rearLeftTransform;
+    [SerializeField] private Transform rearRightTransform;
 
     void FixedUpdate()
     {
-        //HandleMotor();
-        //if (Keyboard.current.spaceKey.isPressed)
-        //{
-        //    //ApplyBreaking();
-        //    Debug.Log("isBereaking");
-        //}
-        Debug.Log(isBreaking);
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
     }
 
     private void HandleMotor()
     {
         frontLeftCollider.motorTorque = moveVec.y * motorForce;
         frontRightCollider.motorTorque = moveVec.y * motorForce;
-
+        if (isBreaking)
+        {
+            ApplyBreaking();
+        }
     }
 
     private void ApplyBreaking()
@@ -65,5 +66,29 @@ public class CarController : MonoBehaviour
     public void OnMove(InputValue value)
     {
         moveVec = value.Get<Vector2>();
+    }
+
+    private void HandleSteering()
+    {
+        currentSteerAngle = maxSteerAngle * moveVec.x;
+        frontLeftCollider.steerAngle = currentSteerAngle;
+        frontRightCollider.steerAngle = currentSteerAngle;
+    }
+
+    private void UpdateWheels()
+    {
+        UpdateSingleWheel(frontLeftCollider, frontLeftTransform);
+        UpdateSingleWheel(frontRightCollider, frontRightTransform);
+        UpdateSingleWheel(rearLeftCollider, rearLeftTransform);
+        UpdateSingleWheel(rearRightCollider, rearRightTransform);
+    }
+
+    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.rotation = rot;
+        wheelTransform.position = pos;
     }
 }
