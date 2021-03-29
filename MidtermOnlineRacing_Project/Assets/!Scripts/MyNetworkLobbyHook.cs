2b2830874c54fc721e5d7440fe245c8b7fe1ce3a
Denpong_Denjarukul <denpong_de@users.noreply.Bitbucket.com>
2021-03-29 12:31:26 +0700

    Add Name to player

---
+++ MidtermOnlineRacing_Project/Assets/!Scripts/MyNetworkLobbyHook.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using Prototype.NetworkLobby;
+
+public class MyNetworkLobbyHook : LobbyHook
+{
+    public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager,
+        GameObject lobbyPlayer, GameObject gamePlayer)
+    {
+        LobbyPlayer lobby = lobbyPlayer.GetComponent<LobbyPlayer>();
+        SetupLocalPlayer actualPlayer = gamePlayer.GetComponent<SetupLocalPlayer>();
+        actualPlayer.pName = lobby.playerName;
+    }
+}
--- MidtermOnlineRacing_Project/Assets/!Scripts/SetupLocalPlayer.cs
+++ MidtermOnlineRacing_Project/Assets/!Scripts/SetupLocalPlayer.cs
@@ -2,9 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class SetupLocalPlayer : NetworkBehaviour
 {
+    public Text namePrefab;
+    public Transform namePos;
+    private Text nameLable;
+    [SyncVar(hook = "OnChangeName")]
+    public string pName = "player";
+
+    private void Awake()
+    {
+        GameObject mainCanvas = GameObject.FindWithTag("MainCanvas");
+
+        nameLable = Instantiate(namePrefab, Vector3.zero, Quaternion.identity) as Text;
+        nameLable.transform.SetParent(mainCanvas.transform);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +38,38 @@ public class SetupLocalPlayer : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 screenPoint = Camera.main.WorldToViewportPoint(this.transform.position);
+        bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 &&
+            screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+        if (onScreen)
+        {
+            Vector3 nameLablePos = Camera.main.WorldToScreenPoint(namePos.position);
+            nameLable.transform.position = nameLablePos;
+        }
+        else
+        {
+            nameLable.transform.position = new Vector3(-1000, -1000, 0);
+        }
+    }
+
+    [Command]
+    public void CmdChangeName(string newName)
+    {
+        pName = newName;
+        nameLable.text = pName;
+    }
+
+    void OnChangeName(string n)
+    {
+        pName = n;
+        nameLable.text = pName;
+    }
+
+    public void OnDestroy()
+    {
+        if (nameLable != null)
+        {
+            Destroy(nameLable.gameObject);
+        }
     }
 }
